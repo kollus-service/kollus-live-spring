e50d205335b11d2900829b8d031957fb398b23fd
jaeyoonlee <jaeyoon.lee@catenoid.net>
2019-07-22 15:29:49 +0900

    2019_07_19 기본틀 작업 중_api에 사용되는 기본 function만 정의.

--- src/main/java/net/catenoid/se/kolluslive/api/Broadcast.java
+++ src/main/java/net/catenoid/se/kolluslive/api/Broadcast.java
@@ -1,4 +1,78 @@
 package net.catenoid.se.kolluslive.api;
 
-public class Broadcast {
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+
+@Component
+public class Broadcast extends ApiExecutor{
+
+    /** Broadcast List 브로드캐스트 리스트 **/
+    public HashMap<String, Object> getBroadcastsList(String keyword, String order, Integer per_page, String start_date,
+                                                     String end_date) throws Exception {
+        String url = String.format("/%s/broadcasts", kollusConfig.getServiceAccount());
+        MultiValueMap<String, String> query = new LinkedMultiValueMap<String, String>();
+        if (keyword != null && !keyword.isEmpty()) query.add("keyword", keyword);
+        if (order != null && !order.isEmpty()) query.add("order", order);
+        if (per_page != null && per_page > 0) query.add("per_page", per_page.toString());
+        if (start_date != null && !start_date.isEmpty()) query.add("start_date", start_date);
+        if (end_date != null && !end_date.isEmpty()) query.add("end_date", end_date);
+        return (HashMap<String, Object>) this.requestGet(url, query);
+    }
+
+    /** Show Broadcast 브로드캐스트 보기 **/
+    public HashMap<String, Object> getShowBroadcasts(String broadcast_key) throws Exception {
+        if (broadcast_key == null && broadcast_key.isEmpty()) return null;
+        String url = String.format("/%s/broadcasts/%s", kollusConfig.getServiceAccount(), broadcast_key);
+
+        return (HashMap<String, Object>) this.requestGet(url, null);
+    }
+
+    /** Update Broadcast 브로드캐스트 수정 **/
+    public HashMap<String, Object> getUpdateBroadcasts(String broadcast_key, String title) throws Exception {
+        String url = String.format("/%s/broadcasts/%s", kollusConfig.getServiceAccount(), broadcast_key);
+
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
+        if (title != null && !title.isEmpty()) requestBody.add("title", title);
+
+        return (HashMap<String, Object>) this.requestPost(url, null, requestBody);
+    }
+
+    /** Remove Broadcast 브로드캐스트 삭제 **/
+    public HashMap<String, Object> getDeleteBroadcasts(String broadcast_key, String title) throws Exception {
+        String url = String.format("/%s/broadcasts/%s", kollusConfig.getServiceAccount(), broadcast_key);
+
+        return (HashMap<String, Object>) this.requestDelete(url);
+    }
+
+    /** Start Duplicate Recording 자동 저장 시작 **/
+    public HashMap<String, Object> getStartBroadcastsRecording(String broadcast_key) throws Exception {
+        String url = String.format("/%s/broadcasts/%s/duplicate-recording", kollusConfig.getServiceAccount(), broadcast_key);
+
+        return (HashMap<String, Object>) this.requestPost(url, null, null);
+    }
+
+    /** Stop Duplicate Recording 자동 저장 종료 **/
+    public HashMap<String, Object> getStopBroadcastsRecording(String broadcast_key, String title) throws Exception {
+        String url = String.format("/%s/broadcasts/%s/duplicate-recording", kollusConfig.getServiceAccount(), broadcast_key);
+
+        return (HashMap<String, Object>) this.requestDelete(url);
+    }
+
+    /** Broadcast List 브로드캐스트 리스트 **/
+    public HashMap<String, Object> getChannelBroadcastsList(String channel_key, String keyword, String order, Integer per_page, String start_date,
+                                                            String end_date) throws Exception {
+        String url = String.format("/%s/channels/%s/broadcasts", kollusConfig.getServiceAccount(), channel_key);
+        MultiValueMap<String, String> query = new LinkedMultiValueMap<String, String>();
+        if (keyword != null && !keyword.isEmpty()) query.add("keyword", keyword);
+        if (order != null && !order.isEmpty()) query.add("order", order);
+        if (per_page != null && per_page > 0) query.add("per_page", per_page.toString());
+        if (start_date != null && !start_date.isEmpty()) query.add("start_date", start_date);
+        if (end_date != null && !end_date.isEmpty()) query.add("end_date", end_date);
+
+        return (HashMap<String, Object>) this.requestGet(url, query);
+    }
+
 }
--- src/main/java/net/catenoid/se/kolluslive/api/Channel.java
+++ src/main/java/net/catenoid/se/kolluslive/api/Channel.java
@@ -25,4 +25,21 @@ public class Channel extends ApiExecutor {
         return (HashMap<String, Object>) this.requestGet(url, query);
 
     }
+
+    /** Create Channel 채널 생성 **/
+    public HashMap<String, Object> getChannels(String title, String customer_code, Integer concurrently_viewer_limit, Integer is_shared,
+                                               String live_media_profile_group_key, String meta_title_pattern, String meta_description,
+                                               Integer recording_file_policy, String recording_file_pattern, Integer recording_file_segment_policy,
+                                               Integer recording_file_segment_by_duration, Integer idle_screen_kind, Integer media_player_policy,
+                                               Integer use_chatting_service, Integer chatting_service_id, String chatting_info, Integer use_duplicate_block,
+                                               Integer use_capture_block, Integer use_timeshift) throws Exception {
+        String url = String.format("/%s/channels", kollusConfig.getServiceAccount());
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
+        if (title != null && !title.isEmpty()) requestBody.add("title", title);
+        if (customer_code != null && !customer_code.isEmpty()) requestBody.add("customer_code", customer_code);
+        if (concurrently_viewer_limit != null && concurrently_viewer_limit > 0) requestBody.add("concurrently_viewer_limit", concurrently_viewer_limit.toString());
+
+        return (HashMap<String, Object>) this.requestPost(url, null, requestBody);
+
+    }
 }
