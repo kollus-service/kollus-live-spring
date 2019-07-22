6e81f2fa719737bd0fce9aa71f4e02dab43930cb
ollehing <ollehing@gmail.com>
2019-07-22 16:31:08 +0900

    2019_07_22 기본틀 작업 중_api에 사용되는 기본 function만 정의.

--- src/main/java/net/catenoid/se/kolluslive/api/Creator.java
+++ src/main/java/net/catenoid/se/kolluslive/api/Creator.java
@@ -1,4 +1,24 @@
 package net.catenoid.se.kolluslive.api;
 
-public class Creator {
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+
+@Component
+public class Creator extends ApiExecutor{
+
+    /** Creator List 크리에이터 리스트
+     * service_account_key : 서비스 어카운트 키
+     * channel_key : 채널 키
+     * keyword : 검색어
+     * **/
+    public HashMap<String, Object> getCreatorList(String channel_key, String keyword) throws Exception {
+        String url = String.format("/%s/channels/%s/creators", kollusConfig.getServiceAccount(), channel_key);
+        MultiValueMap<String, String> query = new LinkedMultiValueMap<String, String>();
+        if (keyword != null && !keyword.isEmpty()) query.add("keyword", keyword);
+
+        return (HashMap<String, Object>) this.requestGet(url, query);
+    }
 }
--- src/main/java/net/catenoid/se/kolluslive/api/ServiceAccount.java
+++ src/main/java/net/catenoid/se/kolluslive/api/ServiceAccount.java
@@ -1,4 +1,28 @@
 package net.catenoid.se.kolluslive.api;
 
-public class ServiceAccount {
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+public class ServiceAccount extends ApiExecutor{
+
+    /** Chatting Service List 채팅 서비스 리스트
+     * service_account_key : 서비스 어카운트 키
+     * **/
+    public HashMap<String, Object> getChattingServiceList() throws Exception {
+        String url = String.format("/%s/chatting-services", kollusConfig.getServiceAccount());
+
+        return (HashMap<String, Object>) this.requestGet(url, null);
+    }
+
+    /** Profile Group List 프로파일 그룹 리스트
+     * service_account_key : 서비스 어카운트 키
+     * **/
+    public HashMap<String, Object> getProfileGroupList() throws Exception {
+        String url = String.format("/%s/profile-groups", kollusConfig.getServiceAccount());
+
+        return (HashMap<String, Object>) this.requestGet(url, null);
+    }
+
 }
